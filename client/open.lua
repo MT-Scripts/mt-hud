985f7a6f6cb9e23dbe7c41629c215a4d3a6562c2
Marttins <89866234+Marttins011@users.noreply.github.com>
2023-12-22 21:36:23 +0000

    Changed file name

--- client/open.lua
+++
@@ -1,6 +0,0 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
-
-function loadPlayerNeeds() -- This is only executed on script start or player load to update the hunger after loaded needs to use the event triggered down here
-    local PlayerData = QBCore.Functions.GetPlayerData()
-    TriggerEvent('hud:client:UpdateNeeds', PlayerData.metadata.hunger, PlayerData.metadata.thirst)
-end
