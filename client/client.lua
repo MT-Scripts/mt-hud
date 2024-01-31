e4eee79e380f7000daa5199db31175879612fd2a
Christian J Bennett <Christianbennett67@gmail.com>
2024-01-31 13:50:53 -0600

    Added 2k and 4k support, remove unnecessary code

--- client/client.lua
+++ client/client.lua
@@ -3,7 +3,7 @@ local playerHUDActive = false
 local hunger = 100
 local thirst = 100
 
-RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     Wait(500)
     startHUD()
 end)
@@ -16,11 +16,14 @@ end)
 
 function startHUD()
     local ped = PlayerPedId()
-    if not IsPedInAnyVehicle(ped) then DisplayRadar(false) else DisplayRadar(true) SendNUIMessage({ action = 'showVehicleHUD' }) end
+    if not IsPedInAnyVehicle(ped) then DisplayRadar(false) else
+        DisplayRadar(true)
+        SendNUIMessage({ action = 'showVehicleHUD' })
+    end
     TriggerEvent('hud:client:LoadMap')
     SendNUIMessage({ action = 'showPlayerHUD' })
     playerHUDActive = true
-    loadPlayerNeeds()
+    LoadPlayerNeeds()
 end
 
 local lastCrossroadUpdate = 0
@@ -74,7 +77,13 @@ CreateThread(function()
                     street2 = crossroads[2],
                     direction = GetDirectionText(GetEntityHeading(vehicle)),
                 })
-            else if vehicleHUDActive then vehicleHUDActive = false DisplayRadar(false) SendNUIMessage({ action = 'hideVehicleHUD' }) end end
+            else
+                if vehicleHUDActive then
+                    vehicleHUDActive = false
+                    DisplayRadar(false)
+                    SendNUIMessage({ action = 'hideVehicleHUD' })
+                end
+            end
         else
             vehicleHUDActive = false
             DisplayRadar(false)
@@ -90,13 +99,13 @@ end)
 
 function GetDirectionText(heading)
     if ((heading >= 0 and heading < 45) or (heading >= 315 and heading < 360)) then
-        return "N"
+        return 'N'
     elseif (heading >= 45 and heading < 135) then
-        return "W"
-    elseif (heading >=135 and heading < 225) then
-        return "S"
+        return 'W'
+    elseif (heading >= 135 and heading < 225) then
+        return 'S'
     elseif (heading >= 225 and heading < 315) then
-        return "E"
+        return 'E'
     end
 end
 
@@ -105,21 +114,21 @@ RegisterNetEvent('hud:client:UpdateNeeds', function(newHunger, newThirst)
     hunger = newHunger
 end)
 
-RegisterNetEvent("hud:client:LoadMap", function()
+RegisterNetEvent('hud:client:LoadMap', function()
     Wait(100)
-    local defaultAspectRatio = 1920/1080
+    local defaultAspectRatio = 1920 / 1080
     local resolutionX, resolutionY = GetActiveScreenResolution()
     local safezone = GetSafeZoneSize()
-    local aspectRatio = (resolutionX-(safezone/2))/(resolutionY-(safezone/2))
+    local aspectRatio = (resolutionX - (safezone / 2)) / (resolutionY - (safezone / 2))
     local minimapOffset = 0
-    if aspectRatio > defaultAspectRatio then minimapOffset = ((defaultAspectRatio-aspectRatio)/3.6)-0.019 end
-    RequestStreamedTextureDict("squaremap", false)
-    if not HasStreamedTextureDictLoaded("squaremap") then Wait(150) end
+    if aspectRatio > defaultAspectRatio then minimapOffset = ((defaultAspectRatio - aspectRatio) / 3.6) - 0.019 end
+    RequestStreamedTextureDict('squaremap', false)
+    if not HasStreamedTextureDictLoaded('squaremap') then Wait(150) end
     SetMinimapClipType(0)
-    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "squaremap", "radarmasksm")
-    AddReplaceTexture("platform:/textures/graphics", "radarmask1g", "squaremap", "radarmasksm")
+    AddReplaceTexture('platform:/textures/graphics', 'radarmasksm', 'squaremap', 'radarmasksm')
+    AddReplaceTexture('platform:/textures/graphics', 'radarmask1g', 'squaremap', 'radarmasksm')
     SetMinimapComponentPosition('minimap', 'L', 'B', 0.0 + minimapOffset, -0.047, 0.1638, 0.183)
-    SetMinimapComponentPosition("minimap_mask", "L", "B", 0.0 + minimapOffset, 0.0, 0.128, 0.20)
+    SetMinimapComponentPosition('minimap_mask', 'L', 'B', 0.0 + minimapOffset, 0.0, 0.128, 0.20)
     SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.00 + minimapOffset, 0.065, 0.252, 0.338)
     SetBlipAlpha(GetNorthRadarBlip(), 0)
     SetMinimapClipType(0)
